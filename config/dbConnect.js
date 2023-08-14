a54ad2e73a9961978230a17ba3dfd8204a288b2c
AbdullahLaham <abed26194@gmail.com>
2023-08-14 12:32:36 +0300

    changes

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose"
 
 export const dbConnect = () => {
     try {
-        const conn = mongoose.connect(process.env.MONGO_URL);
+        const conn = mongoose.connect('mongodb+srv://abed26194:89yzX9QI2lSuFuEL@cluster0.zrkltj7.mongodb.net/?retryWrites=true&w=majority');
         console.log("Database Connected Successully");
     } catch (error) {
         console.log("Database Error");
